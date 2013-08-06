46a8439f1a64d02bc9e74a62f58dde625b80f6fe
Frank <frank@frankbi.com>
2013-08-05 23:00:38 -0400

    Printing simple email template to txt

---
+++ emails_go.py
@@ -0,0 +1,26 @@
+import csv
+
+def messagegen(aList):
+	message = """Dear %s,
+	
+Your email address is %s
+You are %s
+You have %s book(s) checked out
+Wild Wild West
+
+-------------------------------
+""" % (aList[0], aList[1], aList[2], aList[3])
+	return message
+
+with open('addresses.csv', 'rb') as csvfile:
+	read = csv.reader(csvfile)
+	counter = 0
+	for x in read:
+		if counter != 0: # if not header row
+			with open('emails.txt', 'a+') as textfile:
+				textfile.write(messagegen(x))
+		else:
+			counter += 1
+		
+		
+
