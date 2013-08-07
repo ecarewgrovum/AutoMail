28b3153ef275fd6750bf863d478de283fe167c3f
Frank <frank@frankbi.com>
2013-08-07 11:40:05 -0400

    Printing but not numbers

--- emails_go.py
+++ emails_go.py
@@ -2,6 +2,8 @@ import csv
 
 def messagegen(aList):
 	message = """To: %s
+	
+Text: %s
 
 Hi %s
 Thanks so much for taking the time to complete our Chronicle of Philanthropy survey of executive compensation. 
@@ -11,9 +13,9 @@ We're taking active steps this year to ensure the data is double-checked early i
 If you could please take a few minutes to verify the data below, that would help us tremendously. These are the responses that will guide the key components of our analysis and will be printed in the paper and online. 
 
 Chief executive officer in 2011 and 2012: %s, %s
-Total taxable compensation 2011: $%d
-Total nontaxable benefits 2011: $%d
-Total compensation 2011: $ %d
+Total taxable compensation 2011: $%s
+Total nontaxable benefits 2011: $%s
+Total compensation 2011: $ %s + %s + %s
 
 Total taxable compensation 2012: $%d
 Total nontaxable benefits 2012: $%d
@@ -33,7 +35,7 @@ I've also attached a copy of your full responses for your records. Please review
 Many thanks for your assistance, 
 
 -------------------------------
-""" % (aList[4], aList[0], aList[10], aList[3])
+""" % (aList[5], aList[0], aList[1], aList[11], aList[15], aList[59], aList[60], aList[59], aList[60], aList[61])
 	return message
 
 with open('verification_emails_0806.csv', 'rU') as csvfile:
