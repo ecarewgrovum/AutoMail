747a973129caf95cb84b6a3d18310e086f4868f2
Frank <frank@frankbi.com>
2013-08-06 23:57:08 -0400

    Can't tell you what rU is, but it works.

--- emails_go.py
+++ emails_go.py
@@ -12,12 +12,12 @@ Wild Wild West
 """ % (aList[0], aList[1], aList[2], aList[3])
 	return message
 
-with open('addresses.csv', 'rb') as csvfile:
+with open('verification_emails_0806.csv', 'rU') as csvfile:
 	read = csv.reader(csvfile)
 	counter = 0
 	for x in read:
 		if counter != 0: # if not header row
-			with open('emails.txt', 'a+') as textfile:
+			with open('cop_emails.txt', 'a+') as textfile:
 				textfile.write(messagegen(x))
 		else:
 			counter += 1
